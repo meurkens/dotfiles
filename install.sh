bdc6f4229fdf0a35b8001ac4dedfadeef972b6dd
Stijn Meurkens <meurkens@gmail.com>
2016-06-13 20:58:16 +0200

    Big update.

--- install.sh
+++ install.sh
@@ -1,83 +1,82 @@
 #!/usr/bin/env bash
 
 # Ask for the administrator password upfront
+# and keep-alive: update existing `sudo` time stamp until script has finished
 sudo -v
-
-# Keep-alive: update existing `sudo` time stamp until script has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
+echo "Installing xcode..."
 
-# homebrew
-# ========
+xcode-select --install
 
 echo "Installing homebrew..."
 
-xcode-select --install
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew bundle --file=~/.dotfiles/Brewfile > /dev/null
-brew cleanup > /dev/null 2>&1
+brew doctor
+brew update
+brew bundle
+brew cleanup -f
+brew cask cleanup -f
 
+echo "Configuring git..."
 
-# install dotfiles
-# ================
+git config --global user.name "Stijn Meurkens"
+git config --global user.email meurkens@gmail.com
+
+echo "Configuring zsh as default shell..."
+
+if ! grep -q "/usr/local/bin/zsh" /etc/shells; then
+  echo "/usr/local/bin/zsh" | sudo tee -a /etc/shells
+fi
+chsh -s /usr/local/bin/zsh
+
+echo "Creating Code directory..."
+
+mkdir ~/Code
+
+echo "Configuring OS X settings..."
+
+sh ./.osx
 
 echo "Installing dotfiles..."
 
-DOTFILES_DIR=$(pwd -P)
-
-for src in $(find -H "$DOTFILES_DIR" -maxdepth 2 -name '*.symlink' -not -path '*.git*')
+for src in $(find -H "./" -maxdepth 2 -name '*.symlink' -not -path '*.git*')
 do
-  echo "  - .$(basename "${src%.*}")"
-  dst="$HOME/.$(basename "${src%.*}")"
-  ln -sFf "$src" "$dst"
+  dst="~/.$(basename "${src%.*}")"
+  rm -rf "$dst"
+  if [ -f "$dst" -o -d "$dst" -o -L "$dst" ]
+  then
+    ln -s "$src" "$dst"
+  fi
 done
 
-# vim plugins
-# ===========
+# TODO
+# - make instructions to load from github
 
-echo "Installing vim plugins..."
+cat <<--EOF
+# User should do:
+# - Set computer name
+# - Install app store programs
+#   - Pixelmator
+#   - Telephone
+#   - XCode
+#   - Letterspace
+#   - Keynote
+# - Install source code pro font
+#   > https://github.com/adobe-fonts/source-code-pro/releases
+# - Install terminal theme
+# - Install safari extensions
+#   - 1Password
+#   - Pocket
+#   - uBlock
+# - Login to dropbox & sync iTunes music
+#   > rm -rf ~/Music/iTunes
+#   > ln -s ~/Dropbox/Appdata/iTunes ~/Music/
+# - Customize finder pane
+# - Display setting: do not auto adjust brightness
+# - Configure apps
+#   - Spectacles
+#   - Arq
+# - Contacts sync Google
+EOF
 
-vim +PlugInstall +qall!
-
-
-# use zsh
-# =======
-
-echo "Setting ZSH as default shell..."
-
-if ! grep -q "/usr/local/bin/zsh" /etc/shells; then
-  echo "/usr/local/bin/zsh" | tee -a /etc/shells
-fi
-
-# TODO: asks for password, again. :-(
-sudo chsh -s /usr/local/bin/zsh
-
-
-# osx settings
-# ===========
-
-echo "Configuring OS X..."
-
-sh ~/.dotfiles/.osx
-
-
-# homedir
-# =======
-
-# TODO: make sure Dropbox is syncing
-
-echo "Setting up home dir..."
-
-# Link Dropbox music
-rm -rf ~/Music/iTunes
-ln -s ~/Dropbox/Appdata/iTunes ~/Music/
-
-# Create a place to code
-mkdir ~/Code
-
-
-# list stuff that script can't do for me:
-# - install ssh
-# - app store downloads: telephone, pixelmator, xcode, letterspace, keynote
-# - spectacle: keyboard settings
-# - browser plugins: uBlock (origin), Pocket, 1Password
